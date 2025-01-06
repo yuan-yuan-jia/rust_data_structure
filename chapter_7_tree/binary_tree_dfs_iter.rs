11783b52f37ea579674bfc2d6e678b6cb29297c9
jyy <1305081045@qq.com>
2025-01-06 15:04:54 +0000

    feature: visit binary tree in in_order by iteration

--- chapter_7_tree/binary_tree_dfs_iter.rs
+++ chapter_7_tree/binary_tree_dfs_iter.rs
@@ -1,7 +1,12 @@
 use std::{cell::RefCell, collections::VecDeque, rc::Rc};
 
-use data_structure::{include::{print_util, tree_node::{vec_to_tree, TreeNode}}, op_vec};
-
+use data_structure::{
+    include::{
+        print_util,
+        tree_node::{vec_to_tree, TreeNode},
+    },
+    op_vec,
+};
 
 /*前序遍历 */
 fn pre_order(root: &Option<Rc<RefCell<TreeNode>>>) -> Vec<i32> {
@@ -33,6 +38,35 @@ fn pre_order(root: &Option<Rc<RefCell<TreeNode>>>) -> Vec<i32> {
 }
 
 
+
+
+/*中序遍历 */
+fn in_order(root: &Option<Rc<RefCell<TreeNode>>>) -> Vec<i32> {
+  let mut v = Vec::new();
+  if root.is_none() {
+      return v;
+  }
+  let mut stack = VecDeque::new();
+  let mut curr_node = Some(root.as_ref().unwrap().clone());
+
+  while curr_node.is_some() || !stack.is_empty() {
+      while let Some(ref nn) = curr_node {
+          stack.push_back(nn.clone());
+          let node = nn.borrow().left.clone();
+          curr_node = node;
+      }
+
+      if let Some(ref nn) = stack.pop_back() {
+          v.push(nn.borrow().val);
+          curr_node = nn.borrow().right.clone();
+      }else {
+          curr_node = None;
+      }
+  }
+  v
+}
+
+
 fn main() {
  /* 初始化二叉树 */
     // 这里借助了一个从数组直接生成二叉树的函数
@@ -41,7 +75,12 @@ fn main() {
     print_util::print_tree(&root);
 
     /* 前序遍历 */
-    let vec = pre_order(&Some(root));
+    let vec = pre_order(&Some(root.clone()));
     println!("\n前序遍历的节点打印序列 = {:?}", vec);
+
+    /* 中序遍历 */
+    let vec = in_order(&Some(root.clone()));
+    println!("\n前序遍历的节点打印序列 = {:?}", vec);
+
 
 }
